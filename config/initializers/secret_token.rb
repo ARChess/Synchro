bdc1fb3d1c06085347c86a43b52073bdb0a51d33
Ethan McGee <bulletshot60@gmail.com>
2012-10-23 08:43:34 -0400

    initialize rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Synchro::Application.config.secret_token = '03309d726c0b8b50d69e9ebf30677525fbcb617add6a935ebcf02f68219471ff1839785fbf43efcef2b2d72fc900ed0008838a272b419b51b509eef23cc3e28a'
